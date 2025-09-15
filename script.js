0eba6b81c70274348b04eaed70f0ae3ea77cc760
LuccasDutraF <luccas.ferreyro@gmail.com>
2025-09-15 18:34:34 -0300

    Mudando textos e estilos do portfólio

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Theme toggle
 const btn = document.getElementById('themeToggle');
 const saved = localStorage.getItem('theme');
-if(saved === 'light'){ document.documentElement.classList.add('light'); }
-btn.addEventListener('click', ()=>{
+if (saved === 'light') { document.documentElement.classList.add('light'); }
+btn.addEventListener('click', () => {
   document.documentElement.classList.toggle('light');
-  localStorage.setItem('theme', document.documentElement.classList.contains('light') ? 'light':'dark');
+  localStorage.setItem('theme', document.documentElement.classList.contains('light') ? 'light' : 'dark');
 });
